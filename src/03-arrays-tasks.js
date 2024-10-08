74a2db6ee62b10344b10b34c11b019d1f05c379a
warp5t <dmitrij.didencko@yandex.by>
2024-10-08 15:39:50 +0300

    feat: solve distinct()

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -575,8 +575,12 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const result = [];
+  for (let i = start; end >= i; i++) {
+    result.push(i);
+  }
+  return result
 }
 
 /**
